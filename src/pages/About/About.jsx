bd4ed86144d4bcbd67ca7672b689b26b6134e388
Kam <erkamyaman35@gmail.com>
2024-04-24 01:30:13 +0300

    second

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,8 +6,12 @@ export default function About() {
         <div className='about'>
 
             <div className="card-form">
-                <div>a</div>
-                <div>b</div>
+                <div className='techs'></div>
+                <div className='about-me'>
+                    <h1>About Me</h1>
+                    <p>
+                        I'm currently honing my skills as a front-end developer, with a particular interest in building user-friendly interfaces using React. My internship at YAFA Tech is providing me with invaluable hands-on experience, where I'm actively involved in creating visually appealing and interactive web experiences. I'm a passionate learner, constantly seeking to expand my knowledge and explore the latest advancements in front-end development. This pursuit fuels my excitement for the future and the opportunity to contribute my skills to building innovative web applications.</p>
+                </div>
             </div>
         </div>
     )

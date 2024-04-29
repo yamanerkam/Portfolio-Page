bf85f160068b66d2af500134637c2b92c2d6bcde
Kam <erkamyaman35@gmail.com>
2024-04-30 00:46:21 +0300

    form-update

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -5,38 +5,35 @@ export default function Contact() {
     return (
         <div className='contact'>
             <div className='main'>
-                <div>paragraph</div>
+                <div className='parag'>paragraph</div>
                 <div className="form">
                     <form >
-                        <label>
-                            Name:
-                            <input
-                                type="text"
-                                name="name"
-                                required
-                            />
-                        </label>
-                        <label>
-                            Email:
-                            <input
-                                type="email"
-                                name="email"
-                                required
-                            />
-                        </label>
-                        <label>
-                            Message:
-                            <textarea
-                                name="message"
-                                required
-                            />
-                        </label>
-                        <button type="submit">Submit</button>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder='Name'
+                            required
+                        />
+
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder='Email'
+                            required
+                        />
+
+                        <textarea
+                            name="message"
+                            placeholder='Message'
+                            required
+                        />
+
+                        <button className='submit' type="submit">Submit</button>
                     </form>
                 </div>
 
 
             </div>
-        </div>
+        </div >
     )
 }

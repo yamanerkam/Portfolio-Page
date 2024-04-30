0824112feec934ca152b6b0ebc1e89c80b610d9b
Kam <erkamyaman35@gmail.com>
2024-04-30 17:41:11 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/home/Home'
 import About from './pages/About/About'
 import Projects from './pages/Projects/Projects'
 import Contact from './pages/Contact/Contact'
+import Footer from './components/Footer/Footer'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,7 +18,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
-
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Footer.css'
+export default function Footer() {
+    return (
+        <div className='footer'>coded by <span className='kam'>Kam </span>by <span className='emoji'>🪄</span></div>
+    )
+}

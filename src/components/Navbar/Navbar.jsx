b1e41b2d3e259d40afa7bb8dce2e57b1e15da90b
Kam <erkamyaman35@gmail.com>
2024-05-01 22:00:35 +0300

    navbar-responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import './Navbar.css'
-
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import './Navbar.css';
 
 export default function Navbar() {
+    const [showLinks, setShowLinks] = useState(false);
 
     return (
         <div className='navbar'>
-            <span>Kam</span>
-            <ul>
-                <li><a className='link-nav' href="#home" >Home</a></li>
-                <li><a className='link-nav' href="#about">About</a></li>
-                <li><a className='link-nav' href="#projects">Projects</a></li>
-                <li><a className='link-nav' href="#contact">Contact</a>
-                </li>
+            <span className='logo'>Kam</span>
+            <button className='toggle-button' onClick={() => setShowLinks(!showLinks)}>
+                <FaBars className='barr' />
+            </button>
+            <ul className={showLinks ? 'nav-links active' : 'nav-links'}>
+                <li><a className='link-nav' href="#home" onClick={() => setShowLinks(false)}>Home</a></li>
+                <li><a className='link-nav' href="#about" onClick={() => setShowLinks(false)}>About</a></li>
+                <li><a className='link-nav' href="#projects" onClick={() => setShowLinks(false)}>Projects</a></li>
+                <li><a className='link-nav' href="#contact" onClick={() => setShowLinks(false)}>Contact</a></li>
             </ul>
         </div>
-    )
+    );
 }
